7e46732d96e71ac5720d7e1f69f1eff057b9a9c6
Vlado Cingel <vladocingel@gmail.com>
2010-01-02 23:53:38 +0100

    ExtJS and jQuery are now also supported

--- lib/remote_forgery_protection.rb
+++ lib/remote_forgery_protection.rb
@@ -6,25 +6,60 @@ module RemoteForgeryProtection
     
     def javascript_code
       %{
-// Adds authenticity token to all Ajax requests. 
-Ajax.Base.prototype.initialize = Ajax.Base.prototype.initialize.wrap(function() {
-  var args = $A(arguments), proceed = args.shift();
-  args[0] = args[0] || {};
-  var options = args[0];
-  if (!(options.method && options.method.toUpperCase() == "GET")) {
-    var encodedToken = encodeURIComponent(_token);
-    if (Object.isString(options.parameters)) {
-      options.parameters += '&authenticity_token=' + encodedToken;
-    } else if (Object.isHash(options.parameters)) {
-      options.parameters = options.parameters.toObject();
-      options.parameters.authenticity_token = encodedToken;
-    } else {
-      options.parameters = options.parameters || {};
-      options.parameters.authenticity_token = encodedToken;
+// Let's add authenticity token to all Ajax requests. 
+// Prototype
+if (typeof(Prototype) != "undefined") {
+  Ajax.Base.prototype.initialize = Ajax.Base.prototype.initialize.wrap(function() {
+    var args = $A(arguments), proceed = args.shift();
+    args[0] = args[0] || {};
+    var options = args[0];
+    if (!(options.method && options.method.toUpperCase() == "GET")) {
+      var encodedToken = encodeURIComponent(_token);
+      if (Object.isString(options.parameters)) {
+        options.parameters += '&authenticity_token=' + encodedToken;
+      } else if (Object.isHash(options.parameters)) {
+        options.parameters = options.parameters.toObject();
+        options.parameters.authenticity_token = encodedToken;
+      } else {
+        options.parameters = options.parameters || {};
+        options.parameters.authenticity_token = encodedToken;
+      }
     }
-  }
-  proceed.apply(null, args);
-});        
+    proceed.apply(null, args);
+  });
+}
+// ExtJS
+if (typeof(Ext) != "undefined") {
+  Ext.Ajax.on('beforerequest', function(conn, options) {
+    if (!(options.method && options.method.toUpperCase()=="GET")) {
+      if (Ext.isString(options.params)) {
+        options.params = String.format('{0}&authenticity_token={1}', options.params, _token);
+      } else {
+        options.params = options.params || {};
+        options.params.authenticity_token = _token;
+      }
+    }
+  });
+}
+// jQuery
+if (typeof(jQuery) != "undefined") {
+  (function(jQuery){
+    var originalAjax = jQuery.ajax;
+    jQuery.extend({
+      ajax: function(o) {
+        if (!(o.type && o.type.toUpperCase()=="GET")) {
+          o.data = o.data || {};
+          if (typeof(o.data)==="string") {
+            o.data += "&authenticity_token="+_token;
+          } else {
+            o.data.authenticity_token = _token;
+          }
+        }
+        return originalAjax.call(this, o);
+      }
+    });
+  })(jQuery);
+}
       }
     end
     
