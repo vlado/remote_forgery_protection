cc4bf96f18b49a9c871f9fe7f3e1f03b93274352
grosser <grosser.michael@gmail.com>
2012-11-09 12:21:02 -0800

    simplify jquery by using extracted rails.js code

--- lib/remote_forgery_protection.rb
+++ lib/remote_forgery_protection.rb
@@ -45,23 +45,7 @@ if (typeof(Ext) != "undefined") {
 }
 // jQuery
 if (typeof(jQuery) != "undefined") {
-  (function(jQuery){
-    var originalAjax = jQuery.ajax;
-    jQuery.extend({
-      ajax: function(o) {
-        if (!(o.type && o.type.toUpperCase()=="GET")) {
-          o.data = o.data || {};
-          if (typeof(o.data)==="string") {
-            if (o.data.indexOf("authenticity_token") == -1)
-              o.data += "&authenticity_token="+_token;
-          } else {
-            o.data.authenticity_token = _token;
-          }
-        }
-        return originalAjax.call(this, o);
-      }
-    });
-  })(jQuery);
+  $.ajaxPrefilter(function(options, originalOptions, xhr){ if ( !options.crossDomain && _token ) { xhr.setRequestHeader('X-CSRF-Token', _token) }});
 }
       }
     end
